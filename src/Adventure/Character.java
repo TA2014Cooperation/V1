6a1cc5c691a1da76fa0cda63db8ae29cedf74a9d
TA2014 <ragnarok.story@yahoo.de>
2014-03-24 14:35:26 +0100

    Dies und das

--- src/Adventure/Character.java
+++ src/Adventure/Character.java
@@ -8,8 +8,20 @@ public class Character {
 	int VERT;
 	int HP;
 	
+	int[][] Position;
+	
+	
 	int Attack = STR + (10*INT)/STR; 
 	int Abwehr = (int) (VERT + (0.25 * INT));
+	
+	public int[][] getPosition(){
+		
+		
+		
+		
+		return Position;
+	}
+	
 	
 	
 	
--- src/Adventure/Karte.java
+++ src/Adventure/Karte.java
@@ -1,7 +1,63 @@
 package Adventure;
 
 public class Karte {
+	
+	
+	boolean[][] Karte = new boolean[20][20];
+	
+	
+	
+	
+	public String toString(){
+		
+		String puffer = "";
+		
+		for (int i = 0; i< Karte.length; i++){
+			
+			
+			for (int j = 0; j< Karte.length; j++){
+				
+				if (Karte[i][j] == true){
+					
+					puffer = puffer + "O";
+					
+				}
+				
+			}
+			
+			puffer = puffer + "\n";
+			
+		}
+		
+		
+		
+		return puffer;
+	}
+	
+	
+	
+	
+	
+	public Karte(){
+		
+		for (int i = 0; i< Karte.length; i++){
+			
+			
+			for (int j = 0; j< Karte.length; j++){
+				
+				Karte[i][j] = true;
+				
+			}
+		}
+		
+		
+		
+		
+		
+	}
+	
 
-  
+	
+	
 
 }

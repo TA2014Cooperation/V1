1f82e27b30583d50c5ead6dd92f1da015e847396
TA2014 <ragnarok.story@yahoo.de>
2014-03-31 13:46:16 +0200

    Getter und Setter

--- src/Adventure/Character.java
+++ src/Adventure/Character.java
@@ -1,18 +1,73 @@
 package Adventure;
 
 public class Character {
-	
+
 	String Name;
 	int STR;
 	int INT;
 	int VERT;
 	int HP;
-	
+
 	int[][] Position;
 	
-	
-	//int Attack = STR + (10*INT)/STR; 
-	//int Abwehr = (int) (VERT + (0.25 * INT));
-	
 
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public int getSTR() {
+		return STR;
+	}
+
+	public void setSTR(int sTR) {
+		STR = sTR;
+	}
+
+	public int getINT() {
+		return INT;
+	}
+
+	public void setINT(int iNT) {
+		INT = iNT;
+	}
+
+	public int getVERT() {
+		return VERT;
+	}
+
+	public void setVERT(int vERT) {
+		VERT = vERT;
+	}
+
+	public int getHP() {
+		return HP;
+	}
+
+	public void setHP(int hP) {
+		HP = hP;
+	}
+
+	public int[][] getPosition() {
+		return Position;
+	}
+
+	public void setPosition(int[][] position) {
+		Position = position;
+	}
+
+	public int getAttack() {
+
+		int Attack = (int) STR + (10 * INT) / STR;
+		return Attack;
+	}
+
+	public int getAbwehr() {
+
+		int Abwehr = (int) (VERT + (0.25 * INT));
+		return Abwehr;
+	}
 }

c4fbcd970aa3a8f836fb715d6f7ae45b4e3fe768
moritzgerion <moritzgerion@idkfa>
2014-04-02 10:47:27 +0200

    Fehlerbeseitigung

--- src/Adventure/Kampf.java
+++ src/Adventure/Kampf.java
@@ -4,15 +4,13 @@ public class Kampf {
 	
 	//Abwehren 
 	public static boolean abwehren(int STG, int INT, int VERT){
-<<<<<<< HEAD
-		
-		if (STG==0);
+		if (STG==0){
+			
+			
+		}
 		return false;
-=======
-
->>>>>>> branch 'master' of https://github.com/TA2014Cooperation/V1.git
 		
-		return false;
+		
 	}
 	
 	
